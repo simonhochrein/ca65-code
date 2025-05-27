c4c7ef59003386abb6b353981fdad9165e3a89e2
Simon Hochrein <10032418+simonhochrein@users.noreply.github.com>
2025-05-27 15:32:26 -0500

    Fix completion when beginning with `.`

--- client/out/extension.js
+++ client/out/extension.js
@@ -11,17 +11,22 @@ function activate(context) {
     const lsp = context.asAbsolutePath(path.join('client', 'bin', exeName));
     function startServer() {
         const command = vscode_1.workspace.getConfiguration('ca65').get('lsp.path') ?? lsp;
-        console.log(command);
         const serverOptions = { command, transport: node_1.TransportKind.stdio, options: {
-                shell: true
+                shell: true,
+                env: {
+                    RUST_BACKTRACE: 1
+                }
             } };
         const clientOptions = {
             documentSelector: [{ scheme: 'file', language: 'ca65' }],
             synchronize: {
                 fileEvents: vscode_1.workspace.createFileSystemWatcher('**/nes.toml')
-            }
+            },
         };
         client = new node_1.LanguageClient('ca65', 'ca65', serverOptions, clientOptions);
+        client.onProgress(node_1.WorkDoneProgress.type, "ca65", (params) => {
+            console.log(params);
+        });
         client.start();
     }
     vscode_1.workspace.onDidChangeConfiguration(async (event) => {
