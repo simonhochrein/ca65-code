853718be43b8cf8569105989764fa64bf70da14e
techwritescode <10032418+techwritescode@users.noreply.github.com>
2025-08-07 12:01:35 -0500

    Syntax + LSP updates

--- client/out/extension.js
+++ client/out/extension.js
@@ -2,32 +2,44 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
 exports.deactivate = deactivate;
-const path = require("path");
 const vscode_1 = require("vscode");
 const node_1 = require("vscode-languageclient/node");
 let client;
 function activate(context) {
-    const exeName = 'ca65-lsp' + (process.platform == 'win32' ? '.exe' : '');
-    const lsp = context.asAbsolutePath(path.join('client', 'bin', exeName));
+    const exeName = "ca65-lsp" + (process.platform == "win32" ? ".exe" : "");
     function startServer() {
-        const command = vscode_1.workspace.getConfiguration('ca65').get('lsp.path') ?? lsp;
-        const serverOptions = { command, transport: node_1.TransportKind.stdio, options: {
-                shell: true,
-                env: {
-                    RUST_BACKTRACE: 1
+        const command = vscode_1.workspace.getConfiguration("ca65").get("lsp.path");
+        if (command) {
+            const serverOptions = {
+                command,
+                transport: node_1.TransportKind.stdio,
+                options: {
+                    shell: true,
+                    env: {
+                        RUST_BACKTRACE: 1,
+                    },
+                },
+            };
+            console.log(`Starting ca65 language server with command: ${command}`);
+            const clientOptions = {
+                documentSelector: [{ scheme: "file", language: "ca65" }],
+                synchronize: {
+                    fileEvents: vscode_1.workspace.createFileSystemWatcher("**/ca65.toml"),
+                },
+            };
+            client = new node_1.LanguageClient("ca65", "ca65", serverOptions, clientOptions);
+            client.onProgress(node_1.WorkDoneProgress.type, "ca65", (params) => {
+                console.log(params);
+            });
+            client.start();
+        }
+        else {
+            vscode_1.window.showErrorMessage("Failed to find ca65-lsp. Please set server path in extension settings", "Open Settings", "Cancel").then((selected) => {
+                if (selected == "Open Settings") {
+                    vscode_1.commands.executeCommand("workbench.action.openSettings", "ca65.lsp.path");
                 }
-            } };
-        const clientOptions = {
-            documentSelector: [{ scheme: 'file', language: 'ca65' }],
-            synchronize: {
-                fileEvents: vscode_1.workspace.createFileSystemWatcher('**/nes.toml')
-            },
-        };
-        client = new node_1.LanguageClient('ca65', 'ca65', serverOptions, clientOptions);
-        client.onProgress(node_1.WorkDoneProgress.type, "ca65", (params) => {
-            console.log(params);
-        });
-        client.start();
+            });
+        }
     }
     vscode_1.workspace.onDidChangeConfiguration(async (event) => {
         if (event.affectsConfiguration("ca65.lsp.path")) {
@@ -37,12 +49,12 @@ function activate(context) {
             startServer();
         }
     });
-    context.subscriptions.push(vscode_1.commands.registerCommand('ca65.restartLanguageServer', async () => {
+    context.subscriptions.push(vscode_1.commands.registerCommand("ca65.restartLanguageServer", async () => {
         if (client) {
             await client.stop();
         }
         startServer();
-        vscode_1.window.showInformationMessage('Language server restarted.');
+        vscode_1.window.showInformationMessage("Language server restarted.");
     }));
     startServer();
 }
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,72 +1,82 @@
-import * as path from 'path';
-import { workspace, ExtensionContext, window, commands } from 'vscode';
+import * as path from "path";
+import { workspace, ExtensionContext, window, commands, debug } from "vscode";
 
 import {
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-    TransportKind,
-    WorkDoneProgress
-} from 'vscode-languageclient/node';
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+  WorkDoneProgress,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-    const exeName = 'ca65-lsp' + (process.platform == 'win32' ? '.exe' : '')
-    const lsp = context.asAbsolutePath(
-        path.join('client', 'bin', exeName)
-    )
+  const exeName = "ca65-lsp" + (process.platform == "win32" ? ".exe" : "");
 
-    function startServer() {
-        const command = workspace.getConfiguration('ca65').get<string>('lsp.path') ?? lsp;
-        const serverOptions: ServerOptions = { command, transport: TransportKind.stdio, options: {
-            shell: true,
-            env: {
-                RUST_BACKTRACE: 1
+  function startServer() {
+    const command = workspace.getConfiguration("ca65").get<string>("lsp.path");
+    if (command) {
+      const serverOptions: ServerOptions = {
+        command,
+        transport: TransportKind.stdio,
+        options: {
+          shell: true,
+          env: {
+            RUST_BACKTRACE: 1,
+          },
+        },
+      };
+      console.log(`Starting ca65 language server with command: ${command}`);
+
+      const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "ca65" }],
+        synchronize: {
+          fileEvents: workspace.createFileSystemWatcher("**/ca65.toml"),
+        },
+      };
+
+      client = new LanguageClient("ca65", "ca65", serverOptions, clientOptions);
+
+      client.onProgress(WorkDoneProgress.type, "ca65", (params) => {
+        console.log(params);
+      });
+
+      client.start();
+    } else {
+        window.showErrorMessage("Failed to find ca65-lsp. Please set server path in extension settings", "Open Settings", "Cancel").then((selected) => {
+            if (selected == "Open Settings") {
+                commands.executeCommand("workbench.action.openSettings", "ca65.lsp.path");
             }
-        } }
-        
-        const clientOptions: LanguageClientOptions = {
-            documentSelector: [{ scheme: 'file', language: 'ca65' }],
-            synchronize: {
-                fileEvents: workspace.createFileSystemWatcher('**/ca65.toml')
-            },
-        };
-        
-        client = new LanguageClient('ca65', 'ca65', serverOptions, clientOptions);
-
-        client.onProgress(WorkDoneProgress.type, "ca65", (params) => {
-            console.log(params);
         })
-        
-        client.start();
     }
+  }
 
-    workspace.onDidChangeConfiguration(async (event) => {
-        if (event.affectsConfiguration("ca65.lsp.path")) {
-            if (client) {
-                await client.stop();
-            }
-            startServer();
-        }
-    });
-
-    context.subscriptions.push(
-    commands.registerCommand('ca65.restartLanguageServer', async () => {
+  workspace.onDidChangeConfiguration(async (event) => {
+    if (event.affectsConfiguration("ca65.lsp.path")) {
       if (client) {
         await client.stop();
       }
       startServer();
-      window.showInformationMessage('Language server restarted.');
+    }
+  });
+
+  context.subscriptions.push(
+    commands.registerCommand("ca65.restartLanguageServer", async () => {
+      if (client) {
+        await client.stop();
+      }
+      startServer();
+      window.showInformationMessage("Language server restarted.");
     })
   );
 
-    startServer();
+  startServer();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
